52f9058a4dcc4d39bff4f53850277b1e5409cf94
David <xudavidb@gmail.com>
2016-12-11 13:28:01 -0800

    add global yarn path

--- fish/exports.fish
+++ fish/exports.fish
@@ -1,7 +1,6 @@
 set -x PATH /usr/local/bin $PATH
 set -x EDITOR vim
-set -x NVM_DIR "/Users/robo/.nvm"
-set -x KEYTIMEOUT 1
+set -x NVM_DIR "/Users/robo/.nvm" set -x KEYTIMEOUT 1
 set -x SHELL /usr/local/bin/fish
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications --caskroom=/usr/local/Caskroom"
 
@@ -10,3 +9,4 @@ set -x EC2 'ubuntu@184.73.248.188'
 set -x do 'dxu@198.199.119.65'
 set -x gj 'ubuntu@ec2-54-183-131-12.us-west-1.compute.amazonaws.com'
 
+set -x PATH (yarn global bin) $PATH
